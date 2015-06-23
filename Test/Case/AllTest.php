350ae89b7474917afd0b05821559b0c5aab1e805
dogmatic69 <dogmatic69@gmail.com>
2015-06-23 10:27:19 +0100

    initial commit, stripped from infinitas and refactored to work in cake direct

---
+++ Test/Case/AllTest.php
@@ -0,0 +1,14 @@
+<?php
+class AllCakeCsvTestsTest extends PHPUnit_Framework_TestSuite {
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All CakeCsv test');
+		$path = CakePlugin::path('CakeCsv') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+		return $suite;
+	}
+}
